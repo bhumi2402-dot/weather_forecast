0c1a72f683c382abbb65e45ebb8cfb14f653eb09
Bhumika <shahb2006@gmail.com>
2025-08-02 12:26:40 +0530

    first commit

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATICFILES_DIRS = [BASE_DIR / 'static']   # optional, only if your static/ has files used in dev
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
