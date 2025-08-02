e0243ce15fcaef0a202024110a8f794591d545f3
Bhumika <shahb2006@gmail.com>
2025-08-02 12:37:41 +0530

    Collected static files and fixed background image path

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']   # optional, only if your static/ has files used in dev
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
